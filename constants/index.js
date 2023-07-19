b23a348a5b7e787ff0727215f0beca6f778d5622
Mtlz95 <95583549+MTLZ95@users.noreply.github.com>
2023-07-19 17:05:59 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x374d1b80de3205c73f8a9d0e86325e4b7ea3ed59";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
